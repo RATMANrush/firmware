20821960a0dd1aab68afb618f65f3c827aceebd3
ratman_rush <ratman_rush@wp.pl>
2026-05-04 19:47:49 +0200

    gemini to zamot

--- general/package/json-c/json-c.mk
+++
@@ -1,8 +0,0 @@
-JSON_C_VERSION = 0.17
-JSON_C_SITE = https://github.com/json-c/json-c/archive
-JSON_C_SOURCE = $(JSON_C_VERSION).tar.gz
-JSON_C_INSTALL_STAGING = YES
-JSON_C_LICENSE = MIT
-JSON_C_LICENSE_FILES = COPYING
-
-$(eval $(cmake-package))
