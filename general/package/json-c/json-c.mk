f92e869270a4f1bbf718821714a872852a7331b0
ratman_rush <ratman_rush@wp.pl>
2026-05-04 17:05:24 +0200

    nuke json-c patches via hooks

--- general/package/json-c/json-c.mk
+++ general/package/json-c/json-c.mk
@@ -4,5 +4,11 @@
 #
 ################################################################################
 
-# To wymusi zresetowanie listy patchy dla tego pakietu
-JSON_C_PATCH =
+# Całkowite zablokowanie nakładania patchy dla tego pakietu
+JSON_C_PATCH = 
+
+define JSON_C_REMOVE_CUSTOM_PATCHES
+	rm -f $(@D)/0001-null-check-lh-table-lookup.patch
+endef
+
+JSON_C_POST_EXTRACT_HOOKS += JSON_C_REMOVE_CUSTOM_PATCHES
