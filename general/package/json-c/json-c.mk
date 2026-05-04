8205327109843b44ef93e615dcf9ad6ba07b649f
ratman_rush <ratman_rush@wp.pl>
2026-05-04 19:32:01 +0200

    gemini jak zwykle niby niewinny

--- general/package/json-c/json-c.mk
+++ general/package/json-c/json-c.mk
@@ -1,11 +1,8 @@
 JSON_C_VERSION = 0.17
 JSON_C_SITE = https://github.com/json-c/json-c/archive
 JSON_C_SOURCE = $(JSON_C_VERSION).tar.gz
+JSON_C_INSTALL_STAGING = YES
 JSON_C_LICENSE = MIT
 JSON_C_LICENSE_FILES = COPYING
-JSON_C_INSTALL_STAGING = YES
-
-# TO JEST KLUCZOWE: Wymuszamy brak patchy
-JSON_C_PATCH = 
 
 $(eval $(cmake-package))
